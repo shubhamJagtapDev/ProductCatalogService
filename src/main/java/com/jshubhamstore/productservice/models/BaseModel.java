baf0eda5aa7642bc3ac0d0d1b7f36643d9e3ea8d
Shubham Jagtap <shubhamjagtap3777@gmail.com>
2026-01-10 14:12:36 +0530

    Add auto-increment to BaseModel id field
    Annotated the id field in BaseModel with @GeneratedValue(strategy = GenerationType.IDENTITY) to enable automatic incrementing of the primary key. This change ensures that entity IDs are generated by the database.

--- src/main/java/com/jshubhamstore/productservice/models/BaseModel.java
+++ src/main/java/com/jshubhamstore/productservice/models/BaseModel.java
@@ -1,5 +1,7 @@
 package com.jshubhamstore.productservice.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
@@ -10,6 +12,7 @@ import java.util.Date;
 @MappedSuperclass
 public abstract class BaseModel {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO Increment the id
     private Long id;
     private Date createdAt;
     private Date updatedAt;
