806dcd0a55f4e4c2967fcbee96da2ff6bba5b8cb
Shubham Jagtap <shubhamjagtap3777@gmail.com>
2026-01-29 11:32:04 +0530

    Add JPA Repository for Category model

---
+++ src/main/java/com/jshubhamstore/productcatalogservice/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.jshubhamstore.productcatalogservice.repositories;
+
+import com.jshubhamstore.productcatalogservice.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByTitle(String title);
+}
